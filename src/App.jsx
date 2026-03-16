6762e1006079367ceb7de6a02213a5009275e3f8
Orisunmibare Muiz Olamilekan <orismuiz2003@gmail.com>
2026-03-16 19:37:24 +0100

    feat: add service page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import Services from './Pages/Services'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/services' element={<Services />} />
       </Routes>
     </Router>
     </>
---
+++ src/Pages/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../Components/Shared-Components/Nav'
+import Footer from '../Components/Shared-Components/footer'
+import HeroService from '../Components/Heroservice'
+
+export default function Services() {
+  return (
+    <>
+    <Nav /> 
+    <HeroService />
+    <Footer />
+    </>
+  )
+}
