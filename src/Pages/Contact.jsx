2f6f5bedabd8749d1130fd2d90b1721fff601f25
Orisunmibare Muiz Olamilekan <orismuiz2003@gmail.com>
2026-03-19 18:06:26 +0100

    feat(contact): add contact page and ContactSection with full contact UI

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContactSession from '../Components/ContactSection'
+import Nav from '../Components/Shared-Components/Nav'
+import Footer from '../Components/Shared-Components/Footer'
+
+export default function Contact() {
+  return (
+    <>
+    <Nav />
+    <ContactSession />
+    <Footer />
+    </>
+  )
+}
